c5ba83a337a590a54cd6dc9282d96e2750a458f3
Kasper Seweryn <casper@wvffle.net>
2018-11-02 12:04:15 +0100

    Add support for web

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
-const emojicrypt = {
-  table: '😀 😁 😂 🤣 😃 😄 😅 😆 😉 😊 😋 😎 😍 😘 🥰 😗 😙 😚 🙂 🤗 🤩 🤔 🤨 😐 😑 😶 🙄 😏 😣 😥 😮 🤐 😯 😪 😫 😴 😌 😛 😜 😝 🤤 😒 😓 😔 😕 🙃 🤑 😲 🙁 😖 😞 😟 😤 😢 😭 😦 😧 😨 😩 🤯 😬 😰 😱 🥵 🥶 😳 🤪 😵 😡 😠 🤬 😷 🤒 🤕 🤢 🤮 🤧 😇 🤠 🤡 🥳 🥴 🥺 🤥 🤫 🤭 🧐 🤓 😈 👿 👹 👺 💀 👻 👽 🤖 💩 😺 😸 😹 😻 😼 😽 🙀 😿 😾'.split(' '),
-  encrypt (str, startIndex) {
-    const base = btoa(str.replace(/[^\0-~]/g, ch => "%u" + ("000" + ch.charCodeAt().toString(16)).slice(-4) ))
-    if (startIndex < 0) {
-      startIndex = 0
+(() => {
+  const isModule = typeof module !== 'undefined' && 'exports' in module
+
+  const toString = str => isModule ? Buffer.from(str, 'base64').toString() : atob(str)
+  const toBase = str => isModule ? Buffer.from(str).toString('base64') : btoa(str)
+
+  const table = '😀 😁 😂 🤣 😃 😄 😅 😆 😉 😊 😋 😎 😍 😘 🥰 😗 😙 😚 🙂 🤗 🤩 🤔 🤨 😐 😑 😶 🙄 😏 😣 😥 😮 🤐 😯 😪 😫 😴 😌 😛 😜 😝 🤤 😒 😓 😔 😕 🙃 🤑 😲 🙁 😖 😞 😟 😤 😢 😭 😦 😧 😨 😩 🤯 😬 😰 😱 🥵 🥶 😳 🤪 😵 😡 😠 🤬 😷 🤒 🤕 🤢 🤮 🤧 😇 🤠 🤡 🥳 🥴 🥺 🤥 🤫 🤭 🧐 🤓 😈 👿 👹 👺 💀 👻 👽 🤖 💩 😺 😸 😹 😻 😼 😽 🙀 😿 😾'.split(' ')
+
+  const emojicrypt = {
+    encrypt (str, startIndex) {
+      const base = toBase(str.replace(/[^\0-~]/g, ch => "%u" + ("000" + ch.charCodeAt().toString(16)).slice(-4) ))
+      if (startIndex < 0) {
+        startIndex = 0
+      }
+
+      if (startIndex >= table.length) {
+        startIndex = table.length - 1
+      }
+
+      if (!startIndex) {
+        startIndex = Math.floor(Math.random() * table.length)
+      }
+
+      startIndex ^= 0
+
+      return table[startIndex] + base.replace(/./g, c  => {
+        let i = c.charCodeAt(0) - 43 + startIndex
+        if (i >= table.length) i -= table.length
+        const res = table[i]
+        return res ? res : i
+      })
+    },
+    decrypt (str) {
+      const startIndex = table.indexOf(str.slice(0, 2))
+      const base = str.slice(2, str.length).replace(/../g, e => {
+        let i = table.indexOf(e) + 43 - startIndex
+        if (i < 43) i += table.length
+        return String.fromCharCode(i)
+      })
+
+      return unescape(toString(base))
     }
-
-    if (startIndex >= this.table.length) {
-      startIndex = this.table.length - 1
-    }
-
-    if (!startIndex) {
-      startIndex = Math.floor(Math.random() * this.table.length)
-    }
-
-    startIndex ^= 0
-
-    return this.table[startIndex] + base.replace(/./g, c  => {
-      let i = c.charCodeAt(0) - 43 + startIndex
-      if (i >= this.table.length) i -= this.table.length
-      const res = this.table[i]
-      return res ? res : i
-    })
-  },
-  decrypt (str) {
-    const startIndex = this.table.indexOf(str.slice(0, 2))
-    const base = str.slice(2, str.length).replace(/../g, e => {
-      let i = this.table.indexOf(e) + 43 - startIndex
-      if (i < 43) i += this.table.length
-      return String.fromCharCode(i)
-    })
-
-    return unescape(atob(base))
   }
-}
 
-module.exports = emojicrypt
+  if (isModule) {
+    module.exports = emojicrypt
+  }
+})()
